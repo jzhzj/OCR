3713f15ec4d4d8bfe1d885934e0d1a6e4f1e8727
qijiuzhi <35633477+jzhzj@users.noreply.github.com>
2018-07-15 21:24:09 +0800

    prompt wrong file format error

--- src/main/java/com/jzhzj/hocr/App.java
+++ src/main/java/com/jzhzj/hocr/App.java
@@ -9,19 +9,13 @@ import javafx.stage.Stage;
 
 
 public class App extends Application {
-    private static Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("/com/jzhzj/hocr/components/main.fxml"));
         primaryStage.setTitle(MachineProps.APP_NAME);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-    }
-
-    public static Stage getStage() {
-        return stage;
     }
 
     public static void main(String[] args) {
